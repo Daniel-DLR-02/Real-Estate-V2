05c6ebb3591cc9ca966d4e3920e0afde641a2167
Daniel <danidlr02@gmail.com>
2021-11-21 18:56:24 +0100

    Creación del proyecto

---
+++ src/main/java/com/triana/realestatev2/Realestatev2Application.java
@@ -0,0 +1,13 @@
+package com.triana.realestatev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Realestatev2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Realestatev2Application.class, args);
+	}
+
+}
