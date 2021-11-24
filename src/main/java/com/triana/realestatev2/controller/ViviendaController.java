55098c247b8a0da178e4ded35cc10a245b4471d5
Daniel <danidlr02@gmail.com>
2021-11-24 20:12:12 +0100

    feat acabada

--- src/main/java/com/triana/realestatev2/controller/ViviendaController.java
+++ src/main/java/com/triana/realestatev2/controller/ViviendaController.java
@@ -11,10 +11,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -39,6 +39,19 @@ public class ViviendaController {
         }
 
 
+
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<GetViviendaDto>> findAll(){
+
+        List<GetViviendaDto> listaADevolver = new ArrayList<>();
+
+        viviendaService.findAll().stream().forEach(v->{
+            listaADevolver.add(dtoConverter.viviendaToGetViviendaDto(v));
+        });
+
+        return ResponseEntity.ok(listaADevolver);
 
     }
 
