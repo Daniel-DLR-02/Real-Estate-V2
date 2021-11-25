e79b3d859d11e8851d79e6fba370daf87576a553
Daniel De Luna Rodríguez <deluna.rodan21@triana.salesianos.edu>
2021-11-25 12:33:11 +0100

    Ya lista gestores

--- src/main/java/com/triana/realestatev2/service/InmobiliariaService.java
+++ src/main/java/com/triana/realestatev2/service/InmobiliariaService.java
@@ -4,12 +4,23 @@ package com.triana.realestatev2.service;
 import com.triana.realestatev2.model.Inmobiliaria;
 import com.triana.realestatev2.repository.InmobiliariaRepository;
 import com.triana.realestatev2.service.base.BaseService;
+import com.triana.realestatev2.users.model.Usuario;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InmobiliariaService
         extends BaseService<Inmobiliaria,Long, InmobiliariaRepository> {
 
+    public boolean comprobarGestorPerteneceInmobiliaria(Usuario gest, Inmobiliaria inmo){
 
+        boolean pertenece=false;
+
+        for(Usuario gestor : inmo.getGestores()){
+            if(gestor.getId().equals(gest.getId()))
+                pertenece=true;
+        }
+
+        return pertenece;
+    }
 
 }
